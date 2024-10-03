0ef6a1b23c196809b3e1521d2480fb1ab9329a5d
oojeda <ojeda.m.orlando@gmail.com>
2024-10-03 10:54:15 -0400

    Delete product-service-v2 directory

--- product-service-v2/src/infrastructure/webserver/controllers/HealthController.js
+++
@@ -1,7 +0,0 @@
-class HealthController {
-    static getHealth(req, res) {
-        res.status(200).json({ status: ' Test Ok!!!!' });
-    }
-}
-
-module.exports = HealthController;
--- product-service-v2/src/infrastructure/webserver/routes/healthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const HealthController = require('../controllers/HealthController');
-
-const router = express.Router();
-
-router.get('/health', HealthController.getHealth);
-
-module.exports = router;
